d78cc5dd19873c4b8ac34b28bb29e9f3daf64e9a
FNTC Student <fntcstudent@FNTCs-MacBook-Pro.local>
2021-04-10 13:43:18 -0700

    more experimenting, trying out grid and box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, CssBaseline, AppBar, Toolbar } from '@material-ui/core';
+import { Typography, CssBaseline, AppBar, Toolbar, Grid, Box } from '@material-ui/core';
 import { Receipt, Share } from '@material-ui/icons';
 
 
@@ -9,11 +9,32 @@ function App() {
 		<CssBaseline />
 		<AppBar position="relative">
 			<Toolbar>
-				<Receipt />			
-				<Typography variant="h5">Bill Share </Typography>
-				<Share />
+				{/* <Receipt align="center"/>			 */}
+				<Typography variant="h5" align="center">Bill Share </Typography>
+				{/* <Share align="center"/> */}
 			</Toolbar>
 		</AppBar>
+
+
+		<Grid container justify = "center">
+		<Box m="auto">
+		<Typography variant="h5">Profile</Typography>
+		<Typography variant="h5">New Bill</Typography>
+		<Typography variant="h5">Create Group</Typography>
+		<Typography variant="h5">Add Friends</Typography>
+			
+		</Box>
+		</Grid>
+		{/* <Box m="auto">
+			<Box m="auto">
+		<Typography variant="h5">Profile</Typography>
+		<Typography variant="h5">New Bill</Typography>
+		<Typography variant="h5">Create Group</Typography>
+		<Typography variant="h5">Add Friends</Typography>
+			</Box>
+		</Box> */}
+
+
 		</>
 	);
 }
