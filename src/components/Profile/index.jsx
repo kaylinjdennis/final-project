3156b6f92afd2b56b75be4b0caf437d37a8f07ee
Anita <haluska99@hotmail.com>
2021-04-12 17:49:53 -0700

    add icon to profile page

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button, Container, TextField } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import useStyles from '../styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 
 function Profile(props) {
 
+  const classes = useStyles();
 
   return (
-    <>
-      <Typography variant="h3" align="center" justify="center">View profile page</Typography>
-    </>
+
+    <Container component="main" maxWidth="xs" >
+      <div className={classes.paper}>
+        <Avatar className={classes.icon}>
+          <AccountBoxIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Profile
+        </Typography>
+      </div>
+    </Container>
   );
 }
 
