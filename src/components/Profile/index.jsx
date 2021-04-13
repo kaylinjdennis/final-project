9a953948877c6cff35d392a6827e1bd93f958d54
Anita <haluska99@hotmail.com>
2021-04-13 15:43:40 -0700

    add drop down list

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Typography, Button, Container, TextField } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
@@ -10,13 +11,25 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemAvatar';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function Profile(props) {
 
-  const classes = useStyles();
+const classes = useStyles();
 
-  return (
+const [anchorEl, setAnchorEl] = React.useState(null);
 
+const handleClick = (event) => {
+  setAnchorEl(event.currentTarget);
+};
+
+const handleClose = () => {
+  setAnchorEl(null);
+};
+
+return (
+  <>
     <Container component="main" maxWidth="xs" >
       <div className={classes.paper}>
         <Avatar className={classes.icon}>
@@ -25,9 +38,25 @@ function Profile(props) {
         <Typography component="h1" variant="h5">
           Profile
         </Typography>
-      </div>
-    </Container>
-  );
+
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+          Your Bills
+        </Button>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} component={Link} to="/"> Home </MenuItem>
+        <MenuItem onClick={handleClose} component={Link} to="/profile"> Profile</MenuItem>
+        </Menu>
+</div>
+</Container>
+  </>
+);
 }
 
 export default Profile;
