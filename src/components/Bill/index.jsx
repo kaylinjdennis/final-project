6a2c6832b9b1731969b5a913073239d96dc62806
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-04-17 19:28:38 -0600

    Connect page for posed bills to backend

--- src/components/Bill/index.jsx
+++ src/components/Bill/index.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
-import { PieChart, Pie, Cell } from "recharts";
-import { Typography, Container } from '@material-ui/core';
+import { PieChart, Pie, Cell, Tooltip} from "recharts";
+import { Typography, Container} from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import useStyles from '../styles';
@@ -11,24 +11,15 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-
 import useApplicationData from '../../hooks/useApplicationData'
 
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
 
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
+	{ name: 'Paid', value: 4 },
+	{ name: 'Unpaid', value: 3 }
 ];
 
 const COLORS = ['#0088FE', '#00C49F'];
-
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
@@ -37,40 +28,126 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 
   return (
     <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
+      {`$${(percent * 100).toFixed(0)}`}
     </text>
   );
 };
-
 
 function Bill(props) {
 	const billID = props.match.params.id;
 
 	const { state, payBill } = useApplicationData();
 
-	console.log('state.groups', state.groups)
-	console.log('state.friends', state.friends)
-	
-	
+	const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
 
-	const bill = state.bills.received.filter((bill) => bill.id === Number(billID));
+	const receivedBill = state.bills.received.filter((bill) => bill.id === Number(billID));
+	const postedBill = state.bills.posted.filter((bill) => bill.id === Number(billID));
+
+	let bill = [];
+
+	if (receivedBill.length !== 0) {
+		bill = receivedBill;
+	} else if (postedBill.length !== 0) {
+		bill = postedBill;
+	}
+
 	let group = [];
 	let billCreator = [];
-	if (bill.length !== 0) {
+	if (bill.length !== 0 && bill[0].poster_id) {
 		group = state.groups.filter((group) => group.id === bill[0].group_id);
 		billCreator = state.friends.filter((friend) => friend.friend_info.id === bill[0].poster_id);
 	}
 
-	console.log('group', group)
-	console.log('bill', (bill))
+	let groupMembers = [];
+	const paidMembers = [];
+	const owingMembers = [];
+	let paidMembersMapped = [];
+	let owingMembersMapped = [];
 
-  const classes = useStyles();
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
+	if (bill.length !== 0 && bill[0].payee_id) {
+		group = state.groups.filter((group) => group.id === bill[0].group_id);
+		const bills = state.bills.posted.filter((eachBill) => eachBill.invoice_id === bill[0].invoice_id);
+		for (const bill of bills) {
+			groupMembers.push(bill.payee_id);
+			if (bill.paid) {
+				paidMembers.push(bill.payee_id);
+			} else {
+				owingMembers.push(bill.payee_id);
+			}
+		}
+		paidMembersMapped = paidMembers.map(member => {
+			const memberArr = state.friends.filter(friend => friend.friend_info.id === member);
+			console.log('memberObj', memberArr)
+			return (
+				<ListItem>
+          <ListItemAvatar>
+            <Avatar className={classes.friends}
+              src="https://i.pravatar.cc/300"
+            />
+            </ListItemAvatar>
+            <ListItemText
+              primary={memberArr[0].friend_info.name}
+              secondary={secondary ? 'Secondary text' : null}
+            />
+        </ListItem>
+			);
+		})
+		owingMembersMapped = owingMembers.map(member => {
+			const memberArr = state.friends.filter(friend => friend.friend_info.id === member);
+			console.log('memberObj', memberArr)
+			return (
+				<ListItem>
+          <ListItemAvatar>
+            <Avatar className={classes.friends}
+              src="https://i.pravatar.cc/300"
+            />
+            </ListItemAvatar>
+            <ListItemText
+              primary={memberArr[0].friend_info.name}
+              secondary={secondary ? 'Secondary text' : null}
+            />
+        </ListItem>
+			);
+		})
+	}
+
+	console.log('bill:', bill)
+	console.log('paid', paidMembers);
+	console.log('owing', owingMembers)
+
+  // const data = [
+  // 	{ name: 'Paid', value: paidMembers.length },
+  // 	{ name: 'Unpaid', value: owingMembers.length }
+	// ];
+
+// const COLORS = ['#0088FE', '#00C49F'];
+// const RADIAN = Math.PI / 180;
+// const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+//   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+//   const x = cx + radius * Math.cos(-midAngle * RADIAN);
+//   const y = cy + radius * Math.sin(-midAngle * RADIAN);
+// 	console.log('*****', (percent * 100).toFixed(0) * bill[0].cost)
+
+//   return (
+//     <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+//       {`$${percent * bill[0].cost}`}
+//     </text>
+//   );
+// };
 
   return (
     <>
-    { (bill.length === 0 || group.length === 0 || billCreator.length === 0) &&  
+			{bill.length === 0 && 
+				<Typography variant="h5">
+					Loading
+				</Typography>
+			}
+			{(bill.length !== 0 && bill[0].poster_id) && 
+
+		<>
+			{ (group.length === 0 || billCreator.length === 0) &&  
 			<Container component="main" maxWidth="xs">
       <div className={classes.paper}>
         <Avatar className={classes.icon}>
@@ -79,9 +156,6 @@ function Bill(props) {
         <Typography className={classes.heading} component="h1" variant="h5">
           Bill
         </Typography>
-
-        {/* if bill type owing render bill details this way */}
-
         <Grid container alignItems='center' spacing={2} direction='row'>
           <Grid container alignItems='center' item xs={12} sm={12}>
             <Typography component="h1" variant="button">
@@ -112,11 +186,11 @@ function Bill(props) {
           >
             Pay Bill
           </Button>
-    
 				</Grid >
 				</div>
-    </Container> }
-    { (bill.length !== 0 && group.length !== 0 && billCreator.length !== 0) && 
+    </Container> 
+		}
+    { (bill[0].poster_id && bill.length !== 0 && group.length !== 0 && billCreator.length !== 0) && 
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
         <Avatar className={classes.icon}>
@@ -125,9 +199,6 @@ function Bill(props) {
         <Typography className={classes.heading} component="h1" variant="h5">
           Bill
         </Typography>
-
-        {/* if bill type owing render bill details this way */}
-
         <Grid container alignItems='center' spacing={2} direction='row'>
           <Grid container alignItems='center' item xs={12} sm={12}>
             <Typography component="h1" variant="button">
@@ -149,33 +220,68 @@ function Bill(props) {
               $ Due: {bill[0].cost}
             </Typography>
           </Grid>
-          <Button
+							<Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
 						className={classes.submit}
-						onClick={() => {payBill(billID)}}
+            onClick={() => {payBill(billID)}}
+						href={"/profile"}
           >
             Pay Bill
-          </Button>
-    
+          	</Button>
 				</Grid >
-     
 
-        {/* else bill type is owed, render this way */}
-        {/* <Grid container alignItems='center' spacing={2} direction='row'>
-          <Grid container alignItems='center' item xs={12} sm={12}>
+				</div>
+    </Container>
+		}
+				</>
+		}
+	{ (bill.length !== 0 && bill[0].payee_id) &&
+	 <>
+	 	<Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.icon}>
+          <ReceiptIcon />
+        </Avatar>
+        <Typography className={classes.heading} component="h1" variant="h5">
+          Bill
+        </Typography>
+        <Grid container alignItems='center' spacing={2} direction='row'>
+				<Grid container alignItems='center' item xs={12} sm={12}>
             <Typography component="h1" variant="button">
-              Description: 
+              Group: {group[0].name}
             </Typography>
           </Grid>
           <Grid container alignItems='center' item xs={12} sm={12}>
             <Typography component="h1" variant="button">
-              Total:
+              Description: {bill[0].description}
             </Typography>
           </Grid>
-
+          <Grid container alignItems='center' item xs={12} sm={12}>
+            <Typography component="h1" variant="button">
+              Total: ${(bill[0].cost) * groupMembers.length}
+            </Typography>
+          </Grid>
+						<PieChart width={400} height={240} alignItems='flex-start'>
+            	<Pie
+              	data={data}
+              	cx={175}
+              	cy={120}
+              	labelLine={false}
+              	label={renderCustomizedLabel}
+              	outerRadius={100}
+              	fill="#8884d8"
+								dataKey="value"
+								nameKey="name"
+            	>
+              	{data.map((entry, index) => (
+                	<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              	))}
+            	</Pie>
+							{/* <Tooltip /> */}
+          	</PieChart>
           <Grid container spacing={1} direction='row'>
             <Grid item xs={6} md={6} className={classes.billFriends} >
               <Box bgcolor='#0088FE'>
@@ -184,19 +290,7 @@ function Bill(props) {
               </Typography>
               <div className={classes.demo}>
                 <List dense={dense}>
-                  {generate(
-                    <ListItem>
-                    <ListItemAvatar>
-                      <Avatar className={classes.friends}
-                        src="https://i.pravatar.cc/300"
-                      />
-                      </ListItemAvatar>
-                      <ListItemText
-                        primary="friend"
-                        secondary={secondary ? 'Secondary text' : null}
-                      />
-                    </ListItem>,
-                  )}
+                  {paidMembersMapped}
                 </List>
               </div>
               </Box>
@@ -208,46 +302,18 @@ function Bill(props) {
               </Typography>
               <div className={classes.demo}>
                 <List dense={dense}>
-                  {generate(
-                    <ListItem>
-                    <ListItemAvatar>
-                      <Avatar className={classes.friends}
-                        src="https://i.pravatar.cc/300"
-                      />
-                      </ListItemAvatar>
-                      <ListItemText
-                        primary="friend"
-                        secondary={secondary ? 'Secondary text' : null}
-                      />
-                    </ListItem>,
-                  )}
+                  {owingMembersMapped}
                 </List>
               </div>
               </Box>
             </Grid>
           </Grid>
-          <PieChart width={400} height={400} alignItems='flex-start'>
-            <Pie
-              data={data}
-              cx={200}
-              cy={200}
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={100}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-          </PieChart>
-
-        </Grid> */}
-      </div>
-    </Container>
-
+        </Grid> 
+				</div>
+				</Container>
+</>
       }
+    
     </>
   );
 }
--- src/components/styles.js
+++ src/components/styles.js
@@ -2,55 +2,56 @@ import { makeStyles } from '@material-ui/core/styles'
 
 
 const useStyles = makeStyles((theme) => ({
-  button: {
+	button: {
 		margin: theme.spacing(1),
 		// margin: '10px',
 		padding: '10% 10%',
-    fontSize: '30px',
-    // width: '20vw',
-    // height: '10vh'
-    // maxWidth: "lg",
-    // minWidth: "sm"
-    flexGrow: '2'
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  icon: {
-    margin: theme.spacing(2),
-    backgroundColor: theme.palette.primary.main,
-  },
-  form: {
-    width: '100%', 
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  root: {
-    width: '100%',
-    // maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  friends: {
-    height: '100%'
-  },
-  groupForm: {
-    paddingTop: 50,
-  },
-  heading: {
-    marginBottom: 25
-  },
-  barchart: {
-    marginBottom: 25,
-  },
-  billFriends: {
-    marginTop: 25,
-  },
-	
+		fontSize: '30px',
+		// width: '20vw',
+		// height: '10vh'
+		// maxWidth: "lg",
+		// minWidth: "sm"
+		flexGrow: '2'
+	},
+	paper: {
+		marginTop: theme.spacing(5),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	icon: {
+		margin: theme.spacing(2),
+		marginTop: theme.spacing(0),
+		backgroundColor: theme.palette.primary.main,
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+	root: {
+		width: '100%',
+		// maxWidth: 360,
+		backgroundColor: theme.palette.background.paper,
+	},
+	friends: {
+		height: '100%'
+	},
+	groupForm: {
+		paddingTop: 50,
+	},
+	heading: {
+		marginBottom: 25
+	},
+	barchart: {
+		marginBottom: 25,
+	},
+	billFriends: {
+		marginTop: 25,
+	},
+
 }));
 
 export default useStyles;
