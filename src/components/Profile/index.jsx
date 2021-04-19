1938553d8c692500f78e6e60a96d5e8ad46a9ccf
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-04-18 23:33:33 -0600

    More refactoring

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,86 +1,107 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Typography, Button, Container, } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import useStyles from '../styles';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import GroupIcon from '@material-ui/icons/Group';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from "recharts";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import useStyles from "../styles";
+import GroupIcon from "@material-ui/icons/Group";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine,
+} from "recharts";
 
-import useApplicationData from '../../hooks/useApplicationData'
+import useApplicationData from "../../hooks/useApplicationData";
 
 function Profile(props) {
+  const classes = useStyles();
+  const { state } = useApplicationData();
 
-const classes = useStyles();
-const { state } = useApplicationData();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-const [anchorEl, setAnchorEl] = React.useState(null);
-const handleClick = (event) => {
-  setAnchorEl(event.currentTarget);
-};
-const handleClose = () => {
-  setAnchorEl(null);
-};
-
-const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = useState(null);
   const handleClick2 = (event) => {
     setAnchorEl2(event.currentTarget);
   };
   const handleClose2 = () => {
     setAnchorEl2(null);
-	};
-	
-const [anchorEl3, setAnchorEl3] = React.useState(null);
-const handleClick3 = (event) => {
-  setAnchorEl3(event.currentTarget);
-};
-const handleClose3 = () => {
-  setAnchorEl3(null);
-};
+  };
 
-const data = [
-  {
-    name: 'Your Totals',
-    Owed: state.profile_info.total_owed,
-    Owing: state.profile_info.total_owing
-  }
-];
+  const [anchorEl3, setAnchorEl3] = useState(null);
+  const handleClick3 = (event) => {
+    setAnchorEl3(event.currentTarget);
+  };
+  const handleClose3 = () => {
+    setAnchorEl3(null);
+  };
 
-const groups = state.groups.map(group => {
-	const route = `/group/${group.id}`
-	return (
-		<MenuItem onClick={handleClose3} component={Link} to={route}> {group.name} </MenuItem>
-	);
-});
+  const data = [
+    {
+      name: "Your Totals",
+      Owed: state.profile_info.total_owed,
+      Owing: state.profile_info.total_owing,
+    },
+  ];
 
-const billsInDropDown = [];
-const postedBills = state.bills.posted.map((bill) => {
-	if (!billsInDropDown.includes(bill.invoice_id)) {
-		const route = `/bill/${bill.id}`
-		billsInDropDown.push(bill.invoice_id);
-		return (
-			<MenuItem onClick={handleClose} component={Link} to={route}> {bill.description} </MenuItem>
-		);
-	}
-})
+  const groups = state.groups.map((group) => {
+    const route = `/group/${group.id}`;
+    return (
+      <MenuItem onClick={handleClose3} component={Link} to={route}>
+        {" "}
+        {group.name}{" "}
+      </MenuItem>
+    );
+  });
 
-const receivedBills = state.bills.received.map((bill) => {
-	if (!bill.paid) {
-		const route = `/bill/${bill.id}`
-	return (
-		<MenuItem onClick={handleClose2} component={Link} to={route}> {bill.description} </MenuItem>
-	);
-	}
-})
+  const billsInDropDown = [];
+  const postedBills = state.bills.posted.map((bill) => {
+    if (!billsInDropDown.includes(bill.invoice_id)) {
+      const route = `/bill/${bill.id}`;
+      billsInDropDown.push(bill.invoice_id);
+      return (
+        <MenuItem onClick={handleClose} component={Link} to={route}>
+          {" "}
+          {bill.description}{" "}
+        </MenuItem>
+      );
+    }
+  });
 
-return (
-    <Container component="main" maxWidth="xs" >
+  const receivedBills = state.bills.received.map((bill) => {
+    if (!bill.paid) {
+      const route = `/bill/${bill.id}`;
+      return (
+        <MenuItem onClick={handleClose2} component={Link} to={route}>
+          {" "}
+          {bill.description}{" "}
+        </MenuItem>
+      );
+    }
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
         <Avatar className={classes.icon}>
           <AccountBoxIcon />
@@ -88,34 +109,38 @@ return (
         <Typography className={classes.heading} component="h1" variant="h5">
           Profile
         </Typography>
-        <BarChart className={classes.barchart}
-            width={300}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 5,
-              left: 5,
-              bottom: 5
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <ReferenceLine y={0} stroke="#000" />
-            <Bar dataKey="Owed" fill="#8884d8" />
-            <Bar dataKey="Owing" fill="#82ca9d" />
-          </BarChart>
-
-        <Grid container alignItems='center' spacing={2} direction='row'>
-          <Grid container alignItems='center' item xs={12} sm={12}>
+        <BarChart
+          className={classes.barchart}
+          width={300}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 5,
+            left: 5,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="Owed" fill="#8884d8" />
+          <Bar dataKey="Owing" fill="#82ca9d" />
+        </BarChart>
+        <Grid container alignItems="center" spacing={2} direction="row">
+          <Grid container alignItems="center" item xs={12} sm={12}>
             <Typography component="h1" variant="button">
-            <FileCopyIcon fontSize='large'/> Bills Created 
+              <FileCopyIcon fontSize="large" /> Bills Created
             </Typography>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-              <ArrowDropDownIcon fontSize='large' alignItems='center'/>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <ArrowDropDownIcon fontSize="large" alignItems="center" />
             </Button>
             <Menu
               id="simple-menu"
@@ -127,12 +152,16 @@ return (
               {postedBills}
             </Menu>
           </Grid>
-          <Grid container alignItems='center' item xs={12} sm={12}>
+          <Grid container alignItems="center" item xs={12} sm={12}>
             <Typography component="h1" variant="button">
-            < MonetizationOnIcon fontSize='large' /> Bills To Pay
+              <MonetizationOnIcon fontSize="large" /> Bills To Pay
             </Typography>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick2}>
-            <ArrowDropDownIcon fontSize='large'/>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick2}
+            >
+              <ArrowDropDownIcon fontSize="large" />
             </Button>
             <Menu
               id="simple-menu"
@@ -144,13 +173,16 @@ return (
               {receivedBills}
             </Menu>
           </Grid>
-       
-          <Grid container alignItems='flex-end' item xs={12} sm={12}>
+          <Grid container alignItems="flex-end" item xs={12} sm={12}>
             <Typography component="h1" variant="button">
-            <GroupIcon fontSize='large'/> Groups
+              <GroupIcon fontSize="large" /> Groups
             </Typography>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick3}>
-            <ArrowDropDownIcon fontSize='large'/>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick3}
+            >
+              <ArrowDropDownIcon fontSize="large" />
             </Button>
             <Menu
               id="simple-menu"
@@ -163,10 +195,9 @@ return (
             </Menu>
           </Grid>
         </Grid>
-  </div>
-</Container>
-
-);
+      </div>
+    </Container>
+  );
 }
 
 export default Profile;
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useApplicationData() {
 	const [state, setState] = useState({
@@ -9,208 +9,251 @@ export default function useApplicationData() {
 		group_members: [],
 		friends: [],
 		friend_requests: [],
-		profile_info: {}
-	})
+		profile_info: {},
+	});
 
 	useEffect(() => {
-
 		// Update groups and group_members in state
 		getUsersGroups()
-			.then(res => {
+			.then((res) => {
 				for (const group of res) {
-					getGroupMembers(group.id)
-						.then(data => {
-							setState(prev => ({ ...prev, groups: res, group_members: { ...prev.group_members, data } }));
-						})
+					getGroupMembers(group.id).then((data) => {
+						setState((prev) => ({
+							...prev,
+							groups: res,
+							group_members: { ...prev.group_members, data },
+						}));
+					});
 				}
 			})
-			.catch(err => {
-				console.log(err)
+			.catch((err) => {
+				console.log(err);
 			});
 
 		// Update user_id in state to be the current user's id
 		getUser()
-			.then(res => {
-				setState(prev => ({ ...prev, user_id: res }));
+			.then((res) => {
+				setState((prev) => ({ ...prev, user_id: res }));
 				return res;
 			})
 			// Use the user_id to get the current User's posted and recieved bills and update state again
-			.then(res => {
-				return getPostedBills(res)
-					.then(posted => {
-						return getReceivedBills(res)
-							.then(received => {
-								setState(prev => ({
-									...prev,
-									bills: {
-										posted: posted,
-										received: received
-									}
-								}));
-								return res;
-							})
-							.catch(err => {
-								console.log(err)
-							});
-					});
+			.then((res) => {
+				return getPostedBills(res).then((posted) => {
+					return getReceivedBills(res)
+						.then((received) => {
+							setState((prev) => ({
+								...prev,
+								bills: {
+									posted: posted,
+									received: received,
+								},
+							}));
+							return res;
+						})
+						.catch((err) => {
+							console.log(err);
+						});
+				});
 			})
 			// Get the total_owed and total_owing for the user and update state
-			.then(res => {
+			.then((res) => {
 				getProfileInfo(res)
-					.then(res => {
-						setState(prev => ({
+					.then((res) => {
+						setState((prev) => ({
 							...prev,
 							profile_info: {
 								total_owed: res.total_owed,
-								total_owing: -res.total_due
-							}
+								total_owing: -res.total_due,
+							},
 						}));
 					})
-					.catch(err => {
-						console.log(err)
+					.catch((err) => {
+						console.log(err);
 					});
 				return res;
-			}).catch(err => {
-				console.log(err)
+			})
+			.catch((err) => {
+				console.log(err);
 			});
 
 		// Set friends in state with all of the user's current friends
 		getUsersCurrentFriends()
-			.then(res => res.current_friends)
+			.then((res) => res.current_friends)
 			.then((res) => {
-				setState(prev => ({ ...prev, friends: res }));
+				setState((prev) => ({ ...prev, friends: res }));
 			})
-			.catch(err => {
-				console.log(err)
+			.catch((err) => {
+				console.log(err);
 			});
 
 		// Set friend_requests in state with all of the user's recieved friend requests
 		getPendingFriendRequests()
-			.then(res => res.requests_recieved)
+			.then((res) => res.requests_recieved)
 			.then((res) => {
-				setState(prev => ({ ...prev, friends_requests: res }));
+				setState((prev) => ({ ...prev, friends_requests: res }));
 			})
-			.catch(err => {
-				console.log(err)
+			.catch((err) => {
+				console.log(err);
 			});
 	}, []);
 
 	const getUser = () => {
-		return axios.get('/api/user')
-			.then(res => res.data.id)
-	}
+		return axios.get("/api/user").then((res) => res.data.id);
+	};
 
 	const getUsersGroups = () => {
-		return axios.get('/api/groups')
-			.then(res => res.data)
-	}
+		return axios.get("/api/groups").then((res) => res.data);
+	};
 
 	const getUsersCurrentFriends = () => {
-		return axios.get('/api/user/friends')
-			.then(res => {
-				return setState(prev => ({
-					...prev,
-					friends: res.data.current_friends
-				}))
-			})
-	}
+		return axios.get("/api/user/friends").then((res) => {
+			return setState((prev) => ({
+				...prev,
+				friends: res.data.current_friends,
+			}));
+		});
+	};
 
 	const getPendingFriendRequests = () => {
-		return axios.get('/api/user/friends')
-			.then(res => {
-				return setState(prev => ({
-					...prev,
-					friend_requests: res.data.requests_recieved
-				}))
-			})
-	}
+		return axios.get("/api/user/friends").then((res) => {
+			return setState((prev) => ({
+				...prev,
+				friend_requests: res.data.requests_recieved,
+			}));
+		});
+	};
 
 	const getProfileInfo = (userID) => {
-		return axios.get(`/api/user/${userID}`)
-			.then(res => res.data)
-	}
+		return axios.get(`/api/user/${userID}`).then((res) => res.data);
+	};
 
 	const createBill = (cost, description, group_id, includeSelf) => {
-		const bill = { "cost": cost, "description": String(description), "group_id": group_id, "include_self": includeSelf };
+		const bill = {
+			cost: cost,
+			description: String(description),
+			group_id: group_id,
+			include_self: includeSelf,
+		};
 
-		return axios.post('/api/bills', bill)
+		return axios.post("/api/bills", bill).then((res) => {
+			setState((prev) => ({
+				...prev,
+				bills: { ...prev.bills, res },
+			}));
+		});
+	};
+
+	const createGroup = (groupName, members) => {
+		return axios
+			.post("/api/groups", { name: groupName, members: members })
 			.then((res) => {
 				setState((prev) => ({
 					...prev,
-					bills: { ...prev.bills, res }
+					groups: { ...prev.groups, res },
 				}));
-			})
-	}
-
-	const createGroup = (groupName, members) => {
-		return axios.post('/api/groups', { "name": groupName, "members": members })
-			.then(res => {
-				setState(prev => ({
-					...prev,
-					groups: { ...prev.groups, res }
-				}))
-			})
-	}
+			});
+	};
 
 	const sendFriendRequest = (email) => {
-		return axios.post('/api/user/friends', { "friend_info": { "email": email }, "type": "sending" })
-			.then(res => {
-				setState(prev => ({
-					...prev,
-					friends: { ...prev.friends, res }
-				}))
+		return axios
+			.post("/api/user/friends", {
+				friend_info: { email: email },
+				type: "sending",
 			})
-	}
+			.then((res) => {
+				setState((prev) => ({
+					...prev,
+					friends: { ...prev.friends, res },
+				}));
+			});
+	};
 
 	const acceptFriendRequest = (friendInfo) => {
-		return axios.post('/api/user/friends', { "friend_info": { "email": friendInfo.email, "id": friendInfo.id, "name": friendInfo.id, "avatar": friendInfo.avatar }, "type": "accepting" })
-			.then(res => {
-				setState(prev => ({
-					...prev,
-					friends: { ...prev.friends, res }
-				}))
+		return axios
+			.post("/api/user/friends", {
+				friend_info: {
+					email: friendInfo.email,
+					id: friendInfo.id,
+					name: friendInfo.id,
+					avatar: friendInfo.avatar,
+				},
+				type: "accepting",
 			})
-	}
+			.then((res) => {
+				setState((prev) => ({
+					...prev,
+					friends: { ...prev.friends, res },
+				}));
+			});
+	};
 
 	const declineFriendRequest = (friendInfo) => {
-		return axios.post('/api/user/friends', { "friend_info": { "email": friendInfo.email, "id": friendInfo.id, "name": friendInfo.id, "avatar": friendInfo.avatar }, "type": "declining" })
-			.then(res => {
-				setState(prev => ({
-					...prev,
-					friend_requests: { ...prev.friend_requests, res }
-				}))
+		return axios
+			.post("/api/user/friends", {
+				friend_info: {
+					email: friendInfo.email,
+					id: friendInfo.id,
+					name: friendInfo.id,
+					avatar: friendInfo.avatar,
+				},
+				type: "declining",
 			})
-	}
+			.then((res) => {
+				setState((prev) => ({
+					...prev,
+					friend_requests: { ...prev.friend_requests, res },
+				}));
+			});
+	};
 
 	const payBill = (billID) => {
-		return axios.post(`/api/bills/${billID}`, { "paid": true })
-			.then(res => res.data)
-	}
+		return axios
+			.post(`/api/bills/${billID}`, { paid: true })
+			.then((res) => res.data);
+	};
 
 	const deleteBill = (billID) => {
-		return axios.delete(`/api/bills/${billID}`)
-			.then(res => res.data)
-	}
+		return axios.delete(`/api/bills/${billID}`).then((res) => res.data);
+	};
 
 	const editBill = (billID, cost, description, group_id, includeSelf) => {
-		return axios.post(`/api/bills/${billID}`, { "cost": cost, "description": String(description), "group_id": group_id, "include_self": includeSelf })
-			.then(res => res.data)
-	}
+		return axios
+			.post(`/api/bills/${billID}`, {
+				cost: cost,
+				description: String(description),
+				group_id: group_id,
+				include_self: includeSelf,
+			})
+			.then((res) => res.data);
+	};
 
 	const getPostedBills = (userID) => {
-		return axios.get(`/api/bills/posted/${userID}`)
-			.then(res => res.data)
-	}
+		return axios.get(`/api/bills/posted/${userID}`).then((res) => res.data);
+	};
 
 	const getReceivedBills = (userID) => {
-		return axios.get(`/api/bills/received/${userID}`)
-			.then(res => res.data)
-	}
+		return axios.get(`/api/bills/received/${userID}`).then((res) => res.data);
+	};
 
 	const getGroupMembers = (groupID) => {
-		return axios.get(`/api/groups/${groupID}`)
-			.then(res => res.data)
-	}
+		return axios.get(`/api/groups/${groupID}`).then((res) => res.data);
+	};
 
-	return { state, setState, createBill, getUser, getUsersGroups, createGroup, sendFriendRequest, acceptFriendRequest, declineFriendRequest, getPostedBills, getReceivedBills, payBill, deleteBill, editBill, getGroupMembers };
+	return {
+		state,
+		setState,
+		createBill,
+		getUser,
+		getUsersGroups,
+		createGroup,
+		sendFriendRequest,
+		acceptFriendRequest,
+		declineFriendRequest,
+		getPostedBills,
+		getReceivedBills,
+		payBill,
+		deleteBill,
+		editBill,
+		getGroupMembers,
+	};
 }
